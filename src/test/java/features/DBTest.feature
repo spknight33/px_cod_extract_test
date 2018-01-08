dc58a83185cf01a0f13a5ad4e3961766133af406
Steve Knight <stephenknight33@gmail.com>
2018-01-08 12:03:02 +0000

    Added tests for duplicate organisation and check of specific data

--- src/test/java/features/DBTest.feature
+++ src/test/java/features/DBTest.feature
@@ -1,42 +1,56 @@
 Feature: Organisation Extract_Load DB test feature
 
+
   Background: 
+    # Note that these are really pre-conditions 
     Given An initial extract_load run
     And the database is empty
     When extract_load run is completed
 
-  # TODO - volumes to be defined
+  # Exact Volumes will vary depending on which cip extract is used, so the volumes here 
+  # values that should apply to any source 
+  # Regardless of voulmes there will always be more relation records than organisation records
   Scenario: DB test initial load sanity counts
     Then the database contains at least 1 "organisation" records
     And the database contains at least 1 "organisation_relation" records
     And there are more "organisation_relation" records than "organisation"  records
 
-  # AT record type is 27
+  # These are the source types which come from the CIP part of the Cod extract
+  # AT record type is 37
   # PCO record type is 46
   # Contractor type is 8
-  # todo - need to differentiate Dispensing from appliance contrators
   Scenario: DB test initial load record types from db2
-    Then the database contains "AT" record types
-    And the database contains "PCO" record types
-    And the database contains "Contractor" record types
+    Then the database contains "AT" record type
+    And the database contains "PCO" record type
+    And the database contains "Contractor" record type
     
-     Scenario: DB test initial load only record types expected are present
+   # Check that we dont have any other than the expected record types 
+   Scenario: DB test initial load only record types expected are present
     Then the database contains only expected record types
+    
+   Scenario: DB test initial load no duplicate organisation records
+    Then there are no duplicate organisation records
 
-  #And the database contains "Dispensing" and "Appliance" contractors
+  # These are the source types which come from the dpc part of the Cod extract
   # Pharmacy-YP type is arbitary 1337, chosen by developer
   # Pharnacy-Grouping type is arbitary 1338, chosen by developer
   Scenario: DB test initial load record types from DPC
-    Then the database contains "Pharmacy-YP" record types
-    Then the database contains "Pharmacy-Grouping" record types
-
-
+    Then the database contains "Pharmacy-YP" record type
+    Then the database contains "Pharmacy-Grouping" record type
 
   # TODO - check with Andrew if this is always true? including DPC
+  # apparently address line can be null in some case, so cant use this test
   #   Scenario: DB test initial load record data integrity
   #  Then the "organisation" records will have data in all "addressline1" fields
-  # We can check the parent child relations for a typical pharmacy
-  Scenario: DB test specific structure
-    Then the database contains "organisation" record for code "Q98"
-    Then the database contains "organisation" record for code "07Z00"
-    And the database contains "organisation_relation" record for parent "Q98" and child "07Z00"
+  
+  
+  # We can check the parent child relations for a specific pharmacy
+  Scenario Outline: DB test specific structure
+    Then the database contains "<parent_code>" of "<parent_type>"
+    And the database contains "<child_code>" of "<child_type>"
+    And the database contains a relation record between "<parent_code>" and "<child_code>"
+    
+    Examples:
+    | parent_code | parent_type | child_code | child_type |
+    | Q98         | 37          | 07Z00      | 46         |
+    | 07Z00       | 46          | FXXXX      | 8          |
--- src/test/java/stepdefinition/DBTest.java
+++ src/test/java/stepdefinition/DBTest.java
@@ -5,7 +5,6 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
@@ -17,161 +16,241 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class DBTest {
-	
+
 	Logger log = Logger.getLogger(DBTest.class.getName());
-	
-	
+
+	// cip_bet_type mappings for cucumber
 	Map<String, Integer> typeMap = new HashMap<String, Integer>();
 
-	public DBTest()
-	{
-		// load the look up for recordtypes
+	public DBTest() {
+		// load the look up for record types
 		typeMap.put("AT", 37);
 		typeMap.put("PCO", 46);
 		typeMap.put("Contractor", 8);
 		typeMap.put("Pharmacy-YP", 1337);
 		typeMap.put("Pharmacy-Grouping", 1338);
 	}
-	
+
 	@Given("^An initial extract_load run$")
 	public void an_initial_extract_load_run() throws Throwable {
-	    // no ation
+		// no action - pre condition
 	}
-	
+
 	@Given("^the database is empty$")
 	public void the_database_is_empty() throws Throwable {
-	   // no action
+		// no action - pre condition
 	}
-	
+
 	@When("^extract_load run is completed$")
 	public void extract_load_run_is_completed() throws Throwable {
-	   // no action
+		// no action - pre condition
 	}
-	
-	@Then("^the database contains \"([^\"]*)\" record types$")
-	public void the_database_contains_record_types(String recordType) throws Throwable {
-		
-	
-		log.info("contains record type "+ recordType);
-		
-		// lookup 
+
+	@Then("^the database contains \"([^\"]*)\" record type$")
+	public void the_database_contains_record_type(String recordType) throws Throwable {
+
+		log.info("Check DB contains record type: " + recordType);
+
+		// lookup cucumber type for bet_type_id
 		int betType = (int) typeMap.get(recordType);
-		
-		if (betType ==0)
-		{
-			Assert.fail("unknown recordType "+recordType);
+
+		if (betType == 0) {
+			Assert.fail("Unknown recordType: " + recordType);
 		}
-		
-		String check="Select count(*) from organisation org where org.cip_bet_type = "+betType;
-		
+
+		String query = "Select count(*) from organisation org where org.cip_bet_type = " + betType;
+
+		log.info("run query :"+query);
 		Connection connection = BaseDatabaseClass.getConnection();
-		Statement st=connection.createStatement();
-		ResultSet rs= st.executeQuery(check);
-		
+		Statement st = connection.createStatement();
+		ResultSet rs = st.executeQuery(query);
+
 		rs.next();
 		int count = rs.getInt(1);
 		rs.close();
-		
-		log.info("total records on DB:"+count);
-		
-		Assert.assertTrue(count>0, "Expected organisation records of type "+betType+ " but none found");
-		
-		
-	   
+
+		log.info("Total " + recordType+ " records on DB:" + count);
+
+		Assert.assertTrue(count > 0, "Expected organisation records of type " + betType + ", but none found");
 	}
-	
-	
-	
+
 	@Then("^the database contains at least (\\d+) \"([^\"]*)\" records$")
 	public void the_database_contains_at_least_records(int expectedRecordCount, String dbTable) throws Throwable {
-	   
-		log.info("contains at least "+ expectedRecordCount + "records for table: "+dbTable);
-		
-		String check="Select count(*) from " + dbTable;
-		
+
+		log.info("Check DB contains at least " + expectedRecordCount + " records for table: " + dbTable);
+
+		String query = "Select count(*) from " + dbTable;
+
+		log.info("run query :"+query);
 		Connection connection = BaseDatabaseClass.getConnection();
-		Statement st=connection.createStatement();
-		ResultSet rs= st.executeQuery(check);
-		
+		Statement st = connection.createStatement();
+		ResultSet rs = st.executeQuery(query);
+
 		rs.next();
 		int count = rs.getInt(1);
 		rs.close();
-		
-		log.info("total records on DB: "+count);
-		
-		Assert.assertTrue(count>=expectedRecordCount, "Expected records in table "+dbTable+ " to be more than "+expectedRecordCount+" but was "+count);
-		
+
+		log.info("Total records for table "+ dbTable +" on DB: " + count);
+
+		Assert.assertTrue(count >= expectedRecordCount, "Expected records in table " + dbTable + " to be more than "
+				+ expectedRecordCount + ", but was " + count);
+
 	}
-	
+
 	@Then("^there are more \"([^\"]*)\" records than \"([^\"]*)\"  records$")
 	public void there_are_more_records_than_records(String dbTable1, String dbTable2) throws Throwable {
-	    
-		log.info("more records in table "+ dbTable1 + "than records for table"+dbTable2);
-		
-		
-		String check1="Select count(*) from " + dbTable1;
-		String check2="Select count(*) from " + dbTable2;
-		
+
+		log.info("Check there are more records in table " + dbTable1 + " than records for table" + dbTable2);
+
+		String check1 = "Select count(*) from " + dbTable1;
+		String check2 = "Select count(*) from " + dbTable2;
+
 		Connection connection = BaseDatabaseClass.getConnection();
-		Statement st=connection.createStatement();
-		ResultSet rs1= st.executeQuery(check1);
-		
+		Statement st = connection.createStatement();
+		ResultSet rs1 = st.executeQuery(check1);
+
 		rs1.next();
 		int count1 = rs1.getInt(1);
 		rs1.close();
-		
-		ResultSet rs2= st.executeQuery(check2);
-		
+
+		ResultSet rs2 = st.executeQuery(check2);
+
 		rs2.next();
 		int count2 = rs2.getInt(1);
 		rs2.close();
-		
-		log.info("total records on DB table1:"+count1);
-		log.info("total records on DB table2:"+count2);
-		
-		Assert.assertTrue(count1>count2, "Expected more records in table "+dbTable1+ " than in table " + dbTable2);
-		
+
+		log.info("Total records on DB table " + dbTable1 + ": " + count1);
+		log.info("Total records on DB table " + dbTable2 + ": " + count2);
+
+		Assert.assertTrue(count1 > count2, "Expected more records in table " + dbTable1 + " than in table " + dbTable2);
+
 	}
-	
+
 	@Then("^the database contains only expected record types$")
 	public void the_database_contains_only_expected_record_types() throws Throwable {
-		
+
+		// use the lookup map to get all valid cip_bet_types
 		Collection<Integer> typeList = typeMap.values();
 		String query = "Select distinct cip_bet_type from organisation where cip_bet_type not in ";
-	    query = query +  sqlFormattedList(typeList);
-		
-		log.info("sql query is: "+query);
+		query = query + sqlFormattedList(typeList);
+
+		log.info("run query :"+query);
 		Connection connection = BaseDatabaseClass.getConnection();
-		Statement st=connection.createStatement();
-		ResultSet rs= st.executeQuery(query);
-		
-		if (rs.isBeforeFirst())
-		{
+		Statement st = connection.createStatement();
+		ResultSet rs = st.executeQuery(query);
+
+		if (rs.isBeforeFirst()) {
 			// fail as we have record types we are not expecting
-			rs.next();
-			int badType = rs.getInt(1);
-			log.error("Unexpected record type in organisation: "+ badType);
-			Assert.fail("Unexpected cip_bet_types in organisation table :" + badType);
+			String badTypes = buildListFromResultset(rs,1);
+			log.error("Unexpected record type(s) in organisation: " + badTypes);
+			Assert.fail("Unexpected cip_bet_type(s) in organisation table : " + badTypes);
 		}
-		
+
 		rs.close();
-		
-	    
+
+	}
+
+	/**
+	 * Builds a SQL IN clause from the list
+	 * 
+	 * @param typeList
+	 * @return
+	 */
+	private String sqlFormattedList(Collection<Integer> typeList) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("(");
+		for (Integer i : typeList) {
+			sb.append(i + ",");
+		}
+		sb.deleteCharAt(sb.length() - 1);
+		sb.append(")");
+		return sb.toString();
+	}
+
+	@Then("^there are no duplicate organisation records$")
+	public void there_are_no_duplicate_database_organisation_records() throws Throwable {
+
+		String query = "select alt_code from organisation ou where (select count(*) from organisation inr "
+				+ "where inr.alt_code = ou.alt_code) > 1 ";
+
+		log.info("No Duplicate organisation record check: " + query);
+		Connection connection = BaseDatabaseClass.getConnection();
+		Statement st = connection.createStatement();
+		ResultSet rs = st.executeQuery(query);
+
+		if (rs.isBeforeFirst()) {
+			// fail as there are duplicate records - we should have an empty resultset
+			String duplicateCodes = buildListFromResultset(rs,1);
+			log.error("Duplicate records in organisation table: " + duplicateCodes);
+			Assert.fail("Duplicate records in organisation table: "+duplicateCodes);
+		}
+
+		rs.close();
+
 	}
 	
-	private String sqlFormattedList(Collection<Integer> typeList){
-		 StringBuilder sb = new StringBuilder();
-		 sb.append("(");
-		 for (Integer i : typeList){
-		   sb.append(i+",");
-		 }
-		 sb.deleteCharAt(sb.length() -1);
-		 sb.append(")");
-		 return sb.toString();
+	/**
+	 * build a comma delimited list from resultset column
+	 * @param rs
+	 * @param position - used for the get of the result set - ie column
+	 * @return
+	 * @throws Throwable
+	 */
+	private String buildListFromResultset(ResultSet rs,int position) throws Throwable{
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		String i;
+		while(rs.next())
+		{
+			// getstring will work for int columns as well
+			i = rs.getString(position);
+			sb.append(i + ",");
 		}
+		sb.deleteCharAt(sb.length() - 1);
+		sb.append("]");
+		return sb.toString();
+	}
 	
 	
-	
-	
+	@Then("^the database contains \"([^\"]*)\" of \"([^\"]*)\"$")
+	public void the_database_contains_of(String altcode, String type) throws Throwable {
+	    log.info("Check database contains altcode : "+altcode+ ", type: "+ type);
+	    
+	    
+	    String query = "Select * from organisation where alt_code = '" + altcode + "' and cip_bet_type = " + type;
+
+		log.info("run query :"+query);
+		Connection connection = BaseDatabaseClass.getConnection();
+		Statement st = connection.createStatement();
+		ResultSet rs = st.executeQuery(query);
+
+		if (!rs.isBeforeFirst()) {
+			// We must have results  - so empty resultset cause fail
+			
+			Assert.fail("Organisation record for altcode: "+altcode +" and type: " + type + " not found");
+		}
+
+	    
+	    
+	}
+
+	@Then("^the database contains a relation record between \"([^\"]*)\" and \"([^\"]*)\"$")
+	public void the_database_contains_a_relation_record_between_and(String parentCode, String childCode) throws Throwable {
+	    log.info("Check database relation contains parent: "+parentCode + ", child: "+ childCode);
+	    
+	    String query = "Select * from organisation_relation where alt_code_parent = '" + parentCode + "' and alt_code_child = '" + childCode +"'";
+
+		log.info("run query :"+query);
+		Connection connection = BaseDatabaseClass.getConnection();
+		Statement st = connection.createStatement();
+		ResultSet rs = st.executeQuery(query);
+
+		if (!rs.isBeforeFirst()) {
+			// We must have results  - so empty resultset cause fail
+			
+			Assert.fail("Organisation_relation record for parent_alt_code: "+parentCode +" and child_alt_Code: " + childCode + " not found");
+		}
+	    
+	}
 
 }
